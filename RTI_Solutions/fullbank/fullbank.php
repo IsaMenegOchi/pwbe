e4cbe3e12b572964f19203b99e52ab703e910fd2
“IsaMenegOchi” <isabelle.ochini@gmail.com>
2021-08-12 18:17:45 -0300

    Atividade Fullbank finalizada

--- RTI_Solutions/fullbank/fullbank.php
+++ RTI_Solutions/fullbank/fullbank.php
@@ -14,16 +14,29 @@
      
 
         if ($salario <= 5000) {
-            $novoSalario = 20 * $salario/100;
+            $novoSalario = $salario + ($salario * 20/100);
         }
         else {
-            $novoSalario = 20 * $salario/100;
-
+            $novoSalario = $salario + ($salario * 10/100);
         }
-        echo "$novoSalario";
     }
     else {
         echo "Você não preencheu corretamente os campos";
     }
 
 ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link href="style.css" rel="stylesheet"/>
+
+    <title>Document</title>
+</head>
+<body>
+    <p><?="$nome passará a receber R$ $novoSalario, no cargo de $cargo" ?></p>
+</body>
+</html>
