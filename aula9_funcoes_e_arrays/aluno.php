cb9aece0fd07bde1733f85419bea32353085ed45
IsaMenegOchi <isabelle.ochini@gmail.com>
2021-08-27 11:29:49 -0300

    Aula 9 em andamento

---
+++ aula9_funcoes_e_arrays/aluno.php
@@ -0,0 +1,51 @@
+<?php 
+$alunos = [
+    [
+       "nome" => "Kelly",
+       "idade" => 17,
+       "nota" => 100,
+   ],
+   [
+       "nome" => "Paulo",
+       "idade" => 16,
+       "nota" => 85,
+   ],
+   [
+       "nome" => "Gabriel",
+       "idade" => 16,
+       "nota" => 48,
+   ],
+   [
+       "nome" => "Gustavo",
+       "idade" => 16,
+       "nota" => 83,
+   ],
+   [
+       "nome" => "Bruna",
+       "idade" => 16,
+       "nota" => 90,
+   ],
+    [
+        "nome" => "Maria",
+        "idade" => 16,
+        "nota" => 85,
+        ],
+    [
+        "nome" => "Artur",
+        "idade" => 16,
+        "nota" => 82,
+    ],
+    [
+        "nome" => "Gustavo",
+        "idade" => 16,
+        "nota" => 80,
+    ],
+    [
+        "nome" => "Isabela",
+        "idade" => 17,
+        "nota" => 95,
+    ],
+
+];
+
+?>
---
+++ aula9_funcoes_e_arrays/index.php
@@ -0,0 +1,59 @@
+<?php 
+
+//funções que inclui outro arquivo
+// include("aluno.php");
+ //caso o arquivo não existir, ele ainda vai rodar, porem com avisos
+ require("aluno.php");
+ //caso o arquivo não existir, ele quebra a página/ é como um include obrigatório
+ //require_once/include_once - possui as mesmas propriedades anteriores, porem, ele não deixa que coloquemos os mesmos arquivos duas vezes
+
+//importar o arquivo de funções (cria-lo)
+//Chamar a função de "fechar notas"
+//pintar a celula do aprovardo de verde, e de reprovado vermelho
+
+require("funcoes.php");
+
+fecharNota($alunos);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="style.css">
+    
+    <title>Nota dos Alunos</title>
+</head>
+<body>
+    <section>
+    <h1>Tabela de notas</h1>
+    <table>
+        <tr>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th>Nota</th>
+            <th>Situação</th>
+        </tr>
+
+        <?php 
+        foreach($alunos as $aluno){
+        ?>
+        <tr>
+            <td><?= $aluno["nome"]?></td>
+            <td><?= $aluno["idade"]?></td>
+            <td><?= $aluno["nota"]?></td>
+            <td><?= $aluno["situacao"]?></td>
+        </tr>   
+        <?php 
+        }
+        ?>
+        
+    
+    </table>
+    </section>
+</body>
+</html>
