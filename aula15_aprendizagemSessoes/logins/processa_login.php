08e4edc4eb2ddc9dc6d6d2556cd1471da549d039
IsaMenegOchi <isabelle.ochini@gmail.com>
2021-09-23 11:25:13 -0300

    Começando integração de EmpresaX com sessões

---
+++ aula15_aprendizagemSessoes/logins/processa_login.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("funcoes.php");
+
+
+session_start();
+
+
+$dados = lerArquivo("./dados/usuarios.json");
+
+
+    if(isset($_POST["txtUsuario"]) && isset($_POST["senha"])){
+        realizarLogin($_POST["txtUsuario"], $_POST["senha"], $dados);
+    }
+    
+    else if($_GET["logout"]){
+        finalizarLogin();
+    }
+    
+    
+//receber via post os dados do form
+//chamar realizar login 
